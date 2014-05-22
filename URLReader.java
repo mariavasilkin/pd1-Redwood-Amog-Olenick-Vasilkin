8d6ddb05091aaea6ae40004ec30ed283c7cfb457
anjelikalynne <anjelikalynne.amog@cslab1-22.(none)>
2014-05-22 08:36:38 -0400

    possible URL testing reading thing

---
+++ URLReader.java
@@ -0,0 +1,15 @@
+import java.net.*;
+import java.io.*;
+
+public class URLReader {
+    public static void main(String[] args) throws Exception {
+
+        URL oracle = new URL("http://en.wikipedia.org/");
+        BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
+}
