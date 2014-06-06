dccaaec5c7206d5ec3cb8251d3146baab54501b2
anjelikalynne <anjelikalynne.amog@cslab1-14.(none)>
2014-06-06 08:01:24 -0400

    changes to URLReader

--- URLReader.java
+++ URLReader.java
@@ -4,11 +4,21 @@ import java.io.*;
 public class URLReader {
     public static void main(String[] args) throws Exception {
 	//	try {
-	    URL site = new URL("http://www.sosmath.com/calculus/series/convergence/convergence.html");
+	    URL site = new URL("http://docs.oracle.com/javase/tutorial/networking/urls/readingURL.html");
 	    URLConnection con = site.openConnection();
 	    con.connect();
+	    if (con instanceof HttpURLConnection)
+		HttpURLConnection = (HttpURLConnection)con 
 	    InputStream is = con.getInputStream();
+
+	    int status = con.getResponseCode();
+
+	    
+	    if (status >= con.HTTP_BAD_REQUEST)
+		is = con.getErrorStream();
 	    BufferedReader in = new BufferedReader(new InputStreamReader(is));
+	  
+	 
 	    String inputLine = null;
 	    while ((inputLine = in.readLine()) != null)
 		System.out.println(inputLine);
