dde159fc75995a4730743bedf366b804f81ac58d
unknown <m.vasilkin@gmail.com>
2014-06-14 23:19:28 -0400

    HTML stuffs

--- RemoveHTML/HTMLTagExceptions.java
+++ RemoveHTML/HTMLTagExceptions.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 public class HTMLTagExceptions {
     private static ArrayList<String> tags = new ArrayList<String> ();  
+    private static File processedText = new File("text.txt");
       public static void main(String[] args) {
 
 	File IgnoreTags = new File("IgnoreTags.txt");
@@ -20,21 +21,32 @@ public class HTMLTagExceptions {
 	    e.printStackTrace();
 	}
 	
-	for (String s: tags) {
-	    System.out.println(s);
-	}
-      }
-}
+
+      
+
 	
-	/*
 	File FullWiki = new File("Wiki.txt");
 	
 	try {
 	    Scanner sc = new  Scanner(FullWiki).useDelimiter("<");
-	    
+	    FileOutputStream is = new FileOutputStream(processedText);
+	    OutputStreamWriter osw = new OutputStreamWriter(is);
+	    Writer w = new BufferedWriter(osw);
 	    while (sc.hasNextLine()) {
+		boolean isBody = false;
 		String s = sc.next();
-		if ((s.substring(0,2)).equals("i>")) {
-		    while (//basically until theres the close tag, concat to one string
-		    }*/
-    
+		if ((s.substring("0,6")).equals("body>")){
+		    isBody = true;
+		}
+		if (isBody) {
+		    System.out.println(s);
+		}
+	    }
+	    w.write(s);
+	    w.close();
+	}
+	catch (IOException e) {
+	    System.out.println("Error");
+	}
+      }
+}
